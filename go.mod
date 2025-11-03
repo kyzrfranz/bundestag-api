54c78b6b8848c9271bfd695157fc0e37f98f69d2
kyzrfranz <hieber.thomas@gmx.de>
2025-11-03 20:10:21 +0100

    add politiciens in constituency

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/kyzrfranz/bundestag-api
 
-go 1.24
+go 1.25.2
 
 require (
-	github.com/samber/lo v1.49.1
-	golang.org/x/net v0.38.0
+	github.com/samber/lo v1.52.0
+	golang.org/x/net v0.46.0
 )
 
-require golang.org/x/text v0.24.0 // indirect
+require golang.org/x/text v0.30.0 // indirect
--- internal/proxy/constituencies_proxy.go
+++ internal/proxy/constituencies_proxy.go
@@ -3,12 +3,15 @@ package proxy
 import (
 	"encoding/json"
 	"fmt"
-	v1 "github.com/kyzrfranz/bundestag-api/api/v1"
-	myHttp "github.com/kyzrfranz/bundestag-api/internal/http"
-	"github.com/kyzrfranz/bundestag-api/internal/rest"
 	"net/http"
 	"net/url"
 	"strings"
+
+	v1 "github.com/kyzrfranz/bundestag-api/api/v1"
+	myHttp "github.com/kyzrfranz/bundestag-api/internal/http"
+	"github.com/kyzrfranz/bundestag-api/internal/rest"
+	"github.com/kyzrfranz/bundestag-api/pkg/resources"
+	"github.com/samber/lo"
 )
 
 type SearchResult struct {
@@ -20,31 +23,64 @@ type SearchResult struct {
 
 type ConstProxy struct {
 	proxyUrl string
+	repo     resources.Repository[v1.PersonListEntry]
 }
 
-func NewConstituencyProxy(proxyUrl string) *ConstProxy {
-	return &ConstProxy{proxyUrl: proxyUrl}
+func NewConstituencyProxy(proxyUrl string, politiciansRepo resources.Repository[v1.PersonListEntry]) *ConstProxy {
+	return &ConstProxy{proxyUrl: proxyUrl, repo: politiciansRepo}
 }
 
 func (proxy *ConstProxy) ConstituencySearch(w http.ResponseWriter, req *http.Request) {
 	zipcode := req.PathValue("zipcode")
+	constituencies, status := proxy.readConsituencies(zipcode)
+	if status != http.StatusOK {
+		http.Error(w, "Failed to read constituencies", status)
+		return
+	}
 
+	if err := rest.MarshalResponse(w, constituencies); err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (proxy *ConstProxy) ConstituencyPoliticianSearch(w http.ResponseWriter, req *http.Request) {
+	zipcode := req.PathValue("zipcode")
+	constituencies, status := proxy.readConsituencies(zipcode)
+	if status != http.StatusOK {
+		http.Error(w, "Failed to read constituencies", status)
+		return
+	}
+
+	politicians := proxy.repo.List(req.Context())
+	result := lo.Filter(politicians, func(p v1.PersonListEntry, _ int) bool {
+		//filter all p.Constituency.Number iin constituencies
+		return lo.SomeBy(constituencies, func(c v1.Constituency) bool {
+			return c.Number == p.Constituency.Number
+		})
+	})
+
+	if err := rest.MarshalResponse(w, result); err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (proxy *ConstProxy) readConsituencies(zipcode string) ([]v1.Constituency, int) {
 	query, err := url.Parse(fmt.Sprintf("%s?term=%s&_type=query&q=%s", proxy.proxyUrl, zipcode, zipcode))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return nil, http.StatusInternalServerError
 	}
 	data, err := myHttp.FetchUrlAsBrowser(query)
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
+		return nil, http.StatusNotFound
 	}
 	var result SearchResult
 	err = json.Unmarshal(data, &result)
 	if err != nil {
 		fmt.Printf("Failed to unmarshal response: %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return nil, http.StatusInternalServerError
 	}
 
 	var constituencies []v1.Constituency
@@ -54,9 +90,5 @@ func (proxy *ConstProxy) ConstituencySearch(w http.ResponseWriter, req *http.Req
 		constituencies = append(constituencies, v1.Constituency{Number: idParts[0], Name: strings.Trim(strings.Join(parts, ","), " ")})
 	}
 
-	if err = rest.MarshalResponse(w, constituencies); err != nil {
-		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
-		return
-	}
-
+	return constituencies, http.StatusOK
 }
